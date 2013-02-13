d03cbd1fb723c72aaaffa48b698d4d8cfd16309c
Tyler Gaw <me@tylergaw.com>
2013-02-13 10:32:11 -0500

    Commenting out hue rotate for now because its breaking everything

--- blender.js
+++ blender.js
@@ -18,20 +18,25 @@
 });
 
 // Hue Rotate
-[].forEach.call(document.querySelectorAll('[name=hue-change]'), function (el) {
-    var target = document.querySelector(el.getAttribute('data-target')),
-        display = el.parentNode.getElementsByTagName('span')[0];
+// For some reason using hue rotate with blend modes causing the sky to fall.
+// No JS errors are thrown, but all blend modes and hue rotate stops working.
+// Also, the element that both are applied to does odd things like scale up to
+// 200% or so.
+//
+// [].forEach.call(document.querySelectorAll('[name=hue-change]'), function (el) {
+//     var target = document.querySelector(el.getAttribute('data-target')),
+//         display = el.parentNode.getElementsByTagName('span')[0];
 
-    var onchange = function (e) {
-        e.preventDefault();
+//     var onchange = function (e) {
+//         e.preventDefault();
 
-        var val = e.target.value;
-        target.style.webkitFilter = 'hue-rotate' + '(' + val + 'deg)';
-        display.textContent = val;
-    };
+//         var val = e.target.value;
+//         target.style.webkitFilter = 'hue-rotate' + '(' + val + 'deg)';
+//         display.textContent = val;
+//     };
 
-    el.addEventListener('change', onchange, false);
-});
+//     el.addEventListener('change', onchange, false);
+// });
 
 // Show/Hide Toggle
 [].forEach.call(document.querySelectorAll('.show-hide-toggle'), function (el) {
